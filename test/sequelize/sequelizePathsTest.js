43163584dd3ee4acf851fe53df89d870892b0389
Ruben Paz <me@ruben.io>
2016-06-30 02:50:43 +0100

    Fix tests

--- test/sequelize/sequelizePathsTest.js
+++ test/sequelize/sequelizePathsTest.js
@@ -4,7 +4,7 @@ const chai = require('chai');
 const should = chai.should();
 const expect = chai.expect;
 const hapi = require('../serverSetup');
-const hapiPaths = require('../../dist/sequelize/sequelizePaths');
+const hapiPaths = require('../../dist/sequelize/sequelizePaths').default;
 const R = require('ramda');
 const util = require('util');
 
